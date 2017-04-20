9d0df9614f89ee873af2b1c5ec9ef61888d51532
Cathy Trejo <trejo.cathy@gmail.com>
2017-04-19 23:30:00 -0700

    Create Date Object DIY.js

---
+++ Date Object DIY.js
@@ -0,0 +1,7 @@
+var suit = ["Hearts", "Clubs", "Spades", "Diamonds"];
+var face = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+function Card ( suit, face) {
+    this.suit = suit;
+    this.face = face;
+    this.
